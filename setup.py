be39c39c4de97ca36997b50a197692df9682cd33
Chathu283 <e23218@eng.pdn.ac.lk>
2026-06-01 17:37:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="data_inspector",
     version="0.1.0",
-    description="An automated framework for CSV data ingestion, sanitization, and visualization in Colab.",
-    author="Your Name",
+    description="A Modular Data Sanitization & Exploration Engine",
+    author="Ramanayaka R.M.K.D.D.",
     packages=find_packages(),
     install_requires=[
-        "pandas>=1.5.0",
-        "numpy>=1.23.0",
-        "plotly>=5.10.0",
-        "scipy>=1.9.0",
-        "statsmodels>=0.13.0"
+        "pandas",
+        "numpy",
+        "plotly",
+        "scipy"
     ],
-    python_requires=">=3.8",
 )
